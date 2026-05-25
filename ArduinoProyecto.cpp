5ca170e32622d2d3948d2182a570d7fa2a18c2d8
heydianaa <dcmndez21@gmail.com>
2026-05-25 03:15:24 -0600

    Update ArduinoProyecto.cpp

--- ArduinoProyecto.cpp
+++ ArduinoProyecto.cpp
@@ -1,29 +1,31 @@
-//#include <Servo.h>
-//Servo miServo;
+#include <Servo.h>
+Servo miServo;
 
-int boton1 = 2;  //modo
-int boton2 = 3;  //alarma
+const int boton1 = 2; 
+const int boton2 = 16;
 
-int a = 4;
-int b = 5;
-int c = 17;
-int d = 18;
-int e = 19;
-int f = 6;
-int g = 7;
+// Display
+const int a = 4;
+const int b = 5;
+const int c = 17;
+const int d = 18;
+const int e = 15;
+const int f = 6;
+const int g = 7;
 
-int potenciometro = 21;
-int buzzer = 8;
-int servo = 9;
+// Componentes
+const int potenciometro = A5;
+const int buzzer = 8;
+const int servo = 3;
 
-int rojo = 16;
-int azul = 14;
-int verde = 15;
+// RGB
+const int rojo = 9;
+const int verde = 10;
+const int azul = 11;
 
+//variables
 
-//Variables
 int modo = 0;
-int alarmaSilenciada = 0;
 
 int estadoBoton1 = 0;
 int ultimoEstadoBoton1 = 0;
@@ -31,13 +33,13 @@ int ultimoEstadoBoton1 = 0;
 int estadoBoton2 = 0;
 int ultimoEstadoBoton2 = 0;
 
+int alarmaSilenciada = 0;
+
 void setup() {
 
   pinMode(boton1, INPUT);
   pinMode(boton2, INPUT);
-  pinMode(potenciometro, INPUT);
 
-  pinMode(buzzer, OUTPUT);
   pinMode(a, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(c, OUTPUT);
@@ -46,187 +48,234 @@ void setup() {
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
 
+  pinMode(buzzer, OUTPUT);
+
   pinMode(rojo, OUTPUT);
-  pinMode(azul, OUTPUT);
   pinMode(verde, OUTPUT);
+  pinMode(azul, OUTPUT);
+
+  miServo.attach(servo);
+  miServo.write(0);
 }
 
 void loop() {
 
-  // ANTIREBOTE BOTON 1 - CAMBIO DE MODO
+  // boton modo
+
   estadoBoton1 = digitalRead(boton1);
 
   if (estadoBoton1 == HIGH && ultimoEstadoBoton1 == LOW) {
 
-    modo = modo + 1;
+    modo++;
 
-    if (modo == 3) {
+    if (modo > 2) {
       modo = 0;
     }
 
     alarmaSilenciada = 0;
+
     delay(50);
   }
 
   ultimoEstadoBoton1 = estadoBoton1;
 
   //modo 0
+
   if (modo == 0) {
 
-    //display 0
-    digitalWrite(a, HIGH);
-    digitalWrite(b, HIGH);
-    digitalWrite(c, HIGH);
-    digitalWrite(d, HIGH);
-    digitalWrite(e, HIGH);
-    digitalWrite(f, HIGH);
-    digitalWrite(g, LOW);
+    mostrar0();
 
-    //RGB apagado
-    digitalWrite(rojo, LOW);
-    digitalWrite(azul, LOW);
-    digitalWrite(verde, LOW);
+    setColor(0, 0, 0);
 
     digitalWrite(buzzer, LOW);
+
+    miServo.write(0);
   }
 
   //modo 1
-  if (modo == 1) {
 
-    //display 1
-    digitalWrite(a, LOW);
-    digitalWrite(b, HIGH);
-    digitalWrite(c, HIGH);
-    digitalWrite(d, LOW);
-    digitalWrite(e, LOW);
-    digitalWrite(f, LOW);
-    digitalWrite(g, LOW);
+  else if (modo == 1) {
+
+    mostrar1();
 
     int lectura = analogRead(potenciometro);
+
     int lpm = map(lectura, 0, 1023, 0, 200);
+
+    int angulo = map(lpm, 0, 200, 0, 180);
+
+    miServo.write(angulo);
 
     digitalWrite(buzzer, LOW);
 
+    // Bradicardia
     if (lpm < 60) {
-      //amarillo - BRADICARDIA
-      digitalWrite(rojo, LOW);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, HIGH);
+
+      // Amarillo
+      setColor(255, 255, 0);
     }
 
-    else if (lpm >= 60 && lpm <= 100) {
-      //verde - NORMAL
-      digitalWrite(rojo, LOW);
-      digitalWrite(verde, HIGH);
-      digitalWrite(azul, LOW);
+    // Normal
+    else if (lpm <= 100) {
+
+      // Verde
+      setColor(0, 255, 0);
     }
 
-    else if (lpm > 100 && lpm <= 150) {
-      //rojo - TAQUICARDIA
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, LOW);
+    // Taquicardia
+    else if (lpm <= 150) {
+
+      // Rojo
+      setColor(255, 0, 0);
     }
 
-    else if (lpm > 150) {
-      //rojo parpadeante - TAQUICARDIA SEVERA
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, LOW);
+    // Taquicardia severa
+    else {
+
+      setColor(255, 0, 0);
       delay(200);
 
-      digitalWrite(rojo, LOW);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, LOW);
+      setColor(0, 0, 0);
       delay(200);
     }
   }
 
   //modo 2
-  if (modo == 2) {
 
-    //display 2
-    digitalWrite(a, HIGH);
-    digitalWrite(b, HIGH);
-    digitalWrite(c, LOW);
-    digitalWrite(d, HIGH);
-    digitalWrite(e, HIGH);
-    digitalWrite(f, LOW);
-    digitalWrite(g, HIGH);
+  else if (modo == 2) {
+
+    mostrar2();
 
     int lectura = analogRead(potenciometro);
+
     int temperatura = map(lectura, 0, 1023, 20, 45);
 
-    // ANTIREBOTE BOTON 2 - APAGAR ALARMA
+    // alarma
+
     estadoBoton2 = digitalRead(boton2);
 
     if (estadoBoton2 == HIGH && ultimoEstadoBoton2 == LOW) {
+
       alarmaSilenciada = 1;
+
       digitalWrite(buzzer, LOW);
+
       delay(50);
     }
 
     ultimoEstadoBoton2 = estadoBoton2;
 
-    //hipotermia
+    // Hipotermia
     if (temperatura < 35) {
-      //morado
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, HIGH);
+
+      // Morado
+      setColor(255, 0, 255);
+
+      miServo.write(0);
 
       if (alarmaSilenciada == 0) {
         digitalWrite(buzzer, HIGH);
-      } else {
-        digitalWrite(buzzer, LOW);
       }
     }
 
-    //normal
-    else if (temperatura >= 35 && temperatura <= 37) {
-      //cyan
-      digitalWrite(rojo, LOW);
-      digitalWrite(verde, HIGH);
-      digitalWrite(azul, HIGH);
+    // Normal
+    else if (temperatura <= 37) {
+
+      // Cyan
+      setColor(0, 255, 255);
+
+      miServo.write(45);
 
       digitalWrite(buzzer, LOW);
+
       alarmaSilenciada = 0;
     }
 
-    //febricula
-    else if (temperatura > 37 && temperatura <= 38) {
-      //amarillo
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, HIGH);
-      digitalWrite(azul, LOW);
+    // Febricula
+    else if (temperatura <= 38) {
+
+      // Amarillo
+      setColor(255, 255, 0);
+
+      miServo.write(90);
 
       digitalWrite(buzzer, LOW);
+
       alarmaSilenciada = 0;
     }
 
-    //fiebre
-    else if (temperatura > 38 && temperatura <= 39) {
-      //anaranjado / rojo
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, LOW);
-      digitalWrite(azul, LOW);
+    // Fiebre
+    else if (temperatura <= 39) {
+
+      // Naranja
+      setColor(255, 120, 0);
+
+      miServo.write(135);
 
       digitalWrite(buzzer, LOW);
+
       alarmaSilenciada = 0;
     }
 
-    //fiebre alta
-    else if (temperatura > 39) {
-      //blanco
-      digitalWrite(rojo, HIGH);
-      digitalWrite(verde, HIGH);
-      digitalWrite(azul, HIGH);
+    // Fiebre alta
+    else {
+
+      // Blanco
+      setColor(255, 255, 255);
+
+      miServo.write(180);
 
       if (alarmaSilenciada == 0) {
         digitalWrite(buzzer, HIGH);
-      } else {
-        digitalWrite(buzzer, LOW);
       }
     }
   }
+}
+
+//LED RGB
+
+void setColor(int red, int green, int blue) {
+
+  analogWrite(rojo, red);
+  analogWrite(verde, green);
+  analogWrite(azul, blue);
+}
+
+//#0
+
+void mostrar0() {
+
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, LOW);
+}
+
+//#1
+
+void mostrar1() {
+
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+
+//#2
+
+void mostrar2() {
+
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, LOW);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
 }
